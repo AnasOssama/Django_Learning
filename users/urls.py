831f706638410c6daad96bc3a07bd0014591e0d7
AnasOssama <anososama97@gmail.com>
2024-03-02 21:47:58 +0300

    Add users app

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path("",views.index, name="index"),
+    path("login",views.login_view, name="login"),
+    path("login",views.logout_view, name="logout")
+
+]
