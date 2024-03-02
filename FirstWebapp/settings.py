48b5fb561c62f3360d97d8ed7e3203e14b6a897c
AnasOssama <anososama97@gmail.com>
2024-03-02 23:57:31 +0300

    Django_learning project connect database to mysql.

--- FirstWebapp/settings.py
+++ FirstWebapp/settings.py
@@ -83,11 +83,18 @@ WSGI_APPLICATION = 'FirstWebapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+ 
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': env('DATABASE_NAME'),  
+        'USER': env('DATABASE_USER'),  
+        'PASSWORD': env('DATABASE_PASS'),  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+
+    } 
+}  
+
 
 
 # Password validation
